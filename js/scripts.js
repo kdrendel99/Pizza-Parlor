3bad4efcbd5202d8b69de41e4e85ada44c276b3b
Karlson Drendel <kdrendel99@gmail.com>
2021-02-27 15:55:54 -0800

    Build out site functionality aside from one variable initialization bug.

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,25 @@ function Pizza(size, cheese, toppings, extra) {
   this.currentPrice = 10;
 }
 
-Pizza.prototype.toppingsPrice = function(toppings){
-  for(let i=1; i <=this.toppings; i ++){
-    this.currentPrice = this.currentPrice+.25
-    console.log(this.currentPrice);
-  };
-  return this.currentPrice;
-}
+  Pizza.prototype.toppingsPrice = function(toppings){
+    for(let i=1; i <=this.toppings; i ++){
+      this.currentPrice = this.currentPrice+.25
+      console.log(this.currentPrice);
+    };
+    return this.currentPrice;
+  }
+  
+  Pizza.prototype.extrasPrice = function(extra){
+    for(let i=1; i <=this.extra; i ++){
+      this.currentPrice = this.currentPrice+1
+      console.log(this.currentPrice);
+    };
+    return this.currentPrice;
+  } 
 
-Pizza.prototype.extrasPrice = function(extra){
-  for(let i=1; i <=this.extra; i ++){
-    this.currentPrice = this.currentPrice+1
-    console.log(this.currentPrice);
-  };
-  return this.currentPrice;
+finalCost = function(toppings,extra){
+  pizza.toppingsPrice();
+  pizza.extrasPrice();
 }
 
 const pizzaSize = {
@@ -39,20 +44,25 @@ $(document).ready(function() {
     const sizeInput = $("#size").val();
     const cheeseInput = $("#cheese").val();
     const toppingInput = $("input:checkbox[name=toppings]:checked").each(function(){
-      const toppingInput= $(this).val();
-      $('#pizza-toppings').append(toppingInput);
+      const toppingInput = $(this).val();
+      $('#pizza-toppings').append(toppingInput + ", ");
     });
-    
-    let pizza = new Pizza(sizeInput, cheeseInput,toppingInput,"null");
+    const extraInput = $("input:checkbox[name=extra]:checked").each(function(){
+      const extraInput = $(this).val(); $('#pizza-extras').append(extraInput + ", ");
+    });
+
+    let pizzaCost = pizza.finalCost(toppings,extra);
+
+    let pizza = new Pizza(sizeInput, cheeseInput,toppingInput, extraInput);
 
 console.log(pizza);
 
 $("#result").show();
-$("pizza-size").text(pizza.size);
-$("pizza-cheese").text(pizza.cheese);
+$("#pizza-size").text(pizza.size);
+$("#pizza-cheese").text(pizza.cheese);
 // $("pizza-toppings").text(pizza.toppings);
-$("pizza-extras").text(pizza.extra);
-$("pizza-price").text(pizza.currentPrice);
+// $("pizza-extras").text(pizza.extra);
+$("#pizza-price").text(pizzaCost);
   });
 });
 
